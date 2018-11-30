9caf11eefbe5bffb634d763e9832b2390e055db3
Dom <empresslaseen@doms-mbp.lan>
2018-11-29 23:27:34 -0500

    Basic GraphQL set up w/Graphiql

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const expr = require('express');
+const exprGQL = require('express-graphql');
 
 const app = expr();
+
+app.use('/graphql', exprGQL({graphiql: true}))
 
 app.listen(4000, () => {
   console.log('listening on 4000');
