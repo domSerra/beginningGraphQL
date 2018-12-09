e6f1811e0e7691918d75d6db32a4d0f2e4edcfd8
Dom Serra <sig647@gmail.com>
2018-12-09 13:07:48 -0500

    basic GQL backend

--- schema/schema.js
+++ schema/schema.js
@@ -5,17 +5,23 @@ const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 
 
 const CarType = new GraphQLObjectType({
   name: 'Car',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     name: {type: GraphQLString},
-    manufacturer: {type: GraphQLString}
-  }
+    manufacturer: {type: GraphQLString},
+    users: {
+      type: new GraphQLList(UserType),
+      resolve: (parentValue, args) => axios.get(`http://localhost:3000/cars/${parentValue.id}/users`).then(res => res.data)
+    }
+  })
 })
 
 //defines what will be returned by your query
@@ -26,7 +32,7 @@ const UserType = new GraphQLObjectType({
     these are the fields that will be returned under the
     user -- id, firstName, & age
   */
-  fields: {
+  fields: () => ({
     /*
       GraphQLString, int are the data types that need to be
       fulfilled in order to allow the query to be successful
@@ -38,7 +44,7 @@ const UserType = new GraphQLObjectType({
       type: CarType,
       resolve: (parentValue, args) => axios.get(`http://localhost:3000/cars/${parentValue.carId}`).then(res => res.data)
     }
-  }
+  })
 });
 
 //the root query is the 'entry point' for GraphQL
@@ -87,10 +93,47 @@ const RootQuery = new GraphQLObjectType({
         API/data calls will be made in here within the promise.
       */
       resolve: (parentValue, args) => axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data)
+    },
+    car: {
+      type: CarType,
+      args: {id: {type: GraphQLString}},
+      resolve: (parentValue, args) => axios.get(`http://localhost:3000/cars/${args.id}`).then(res => res.data)
     }
   }
 })
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)},
+        carId: {type: GraphQLString}
+      },
+      resolve: (parentValue, {firstName, age}) => axios.post('http://localhost:3000/users', {firstName, age}).then(res => res.data)
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve: (parentValue, {id}) => axios.delete(`http://localhost:3000/users/${id}`).then(res => res.data)
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+        firstName: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        carId: {type: GraphQLString}
+      },
+      resolve: (parentValue, args) => axios.patch(`http://localhost:3000/users/${args.id}`, args).then(res => res.data)
+    }
+  }
+})
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 })
